68846cdafa2b74f41eb964b37e30ca000a2ef798
lumana <luis.umanasalas@gmail.com>
2017-03-29 13:09:27 -0600

    Avance hasta lección 31

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -7,16 +8,31 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context; //Para usar EntityFramework
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ViewResult Index()
         {
-            var customers = GetCustomers();
+            //var customers = GetCustomers();
+            //El include hace uso de Eager loading, sirve oara cargar los customers y los memberships types juntos
+            var customers = _context.Customer.Include(c => c.MembershipType).ToList(); //Con esto se obtienen todos los customers de la base de datos
 
             return View(customers);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            //var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customer.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
@@ -24,13 +40,13 @@ namespace Vidly.Controllers
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomers()
+        /*private IEnumerable<Customer> GetCustomers()
         {
             return new List<Customer>
             {
                 new Customer { Id = 1, Name = "John Smith" },
                 new Customer { Id = 2, Name = "Mary Williams" }
             };
-        }
+        }*/
     }
 } 
--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,9 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+        /*Uso de data anotations en la columna de Name*/
+        [Required] // Con este atributo la columna de nombre no va a ser nullable
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
